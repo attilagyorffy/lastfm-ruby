a00444e3e54c5c19c6c2478948bfc706984bb07c
Attila Györffy <attila.gyorffy@gmail.com>
2009-08-05 02:23:46 +0100

    added example file

--- example.rb
+++ example.rb
@@ -1,6 +1,6 @@
 require 'lib/lastfm-ruby'
 
-# You can add your own API key here.
+# You can add your own API key here. You can apply for an API key at http://www.last.fm/api/account
 Lastfm::API_KEY = 'b25b959554ed76058ac220b7b2e0a026'
 
 artist = Lastfm::Artist.new('Noisia')
